685c0d14e9094c747ecc68b4276852d84c1f1086
Vinicius2026 <jasvinigaming@gmail.com>
2025-06-09 17:50:52 -0300

    Fix Favicon 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
 import { AuthProvider } from "@/hooks/useCustomAuth";
-=======
-import { AuthProvider } from "@/hooks/useAuth";
->>>>>>> 6c1222520196260cabbb9b0cc5dfbd5944c51f3e
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
